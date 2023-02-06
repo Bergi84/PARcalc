cbef3e85d452a2d922ecee9ba50367dd28a15b9e
Bergi84 <Klaus.Bergkirchner@gmail.com>
2023-02-06 21:18:01 +0100

    wip

--- main.py
+++ main.py
@@ -10,27 +10,38 @@ lumEffX, lumEffY = np.split(np.transpose(np.genfromtxt("luminousEfficiency.csv")
 lumEffX = np.ravel(lumEffX)
 lumEffY = np.ravel(lumEffY)
 
-
+lumEff_Sp = make_interp_spline(lumEffX, lumEffY, k=3)
 
 # read relative spectral power density
-spd_X, lm_spd_Y = np.split(np.transpose(np.genfromtxt("CREE_J2835_4000K 90CRI.csv")), 2)
+spd_X, spd_Y = np.split(np.transpose(np.genfromtxt("CREE_J2835_4000K 90CRI.csv")), 2)
 spd_X = np.ravel(spd_X)
-lm_spd_Y = np.ravel(lm_spd_Y)
+spd_Y = np.ravel(spd_Y)
 
-lm_spd_Sp = make_interp_spline(spd_X, lm_spd_Y, k=3)
-lm_spd_Y = lm_spd_Sp(lumEffX)
+spd_Sp = make_interp_spline(spd_X, spd_Y, k=3)
+
+# interpolate both cures to fixed X values
+X = np.linspace(np.min(spd_X), np.max(spd_X), 1000)
+lumEff_Y = lumEff_Sp(X)
+spd_Y = spd_Sp(X)
 
 # normalize relative spectral power density
-intSPD = np.trapz(lm_spd_Y, lumEffX)
-lm_spd_Y_norm = lm_spd_Y/intSPD
+intSPD = np.trapz(spd_Y, X)
+spd_Y_norm = spd_Y/intSPD
 
 # multiply spd with luminous efficacy and lumen per Watt
 # to get the power (photonic power, not electrical) to lumen efficiency for this lamp
-p_spd_Y = np.multiply(lm_spd_Y_norm, lumEffY * lumPerWatt)
-lmPerW = np.trapz(p_spd_Y, lumEffX)
+spd_lm_Y = np.multiply(spd_Y_norm, lumEff_Y * lumPerWatt)
+lmPerW = np.trapz(spd_lm_Y, X)
 
 print("%.2f" % lmPerW, " lm/W")
 
+plt.subplot(3,1,1)
+plt.plot(X, lumEff_Y)
 
-plt.plot(lumEffX, p_spd_Y)
+plt.subplot(3,1,2)
+plt.plot(X, spd_Y)
+
+plt.subplot(3,1,3)
+plt.plot(X, spd_lm_Y)
+
 plt.show()
